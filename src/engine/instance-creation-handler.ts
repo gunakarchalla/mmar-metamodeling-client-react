4b8f79365b0a24c8d73350d8c80f3c58ce6dd621
Gunakar Challa <gunakarchalla@gmail.com>
2026-09-06 11:43:54 +0000

    eslint upgrade

--- src/engine/instance-creation-handler.ts
+++ src/engine/instance-creation-handler.ts
@@ -137,7 +137,7 @@ export class InstanceCreationHandler {
   //-------------------------------------------------
   // bendpoint_instance
   //-------------------------------------------------
-  // eslint-disable-next-line @typescript-eslint/ban-types -- faithful port: the original geometry param is typed `Function` (only `.toString()` is called on it)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type -- faithful port: the original geometry param is typed `Function` (only `.toString()` is called on it)
   async createBendpointInstance(x: number, y: number, z: number, geometry: Function, bendPointClassUUID: string) {
     //we round the position
     x = Math.round(x * 10) / 10;
@@ -373,8 +373,8 @@ export class InstanceCreationHandler {
     const port_instance = new PortInstance(uuid, metaPortUUID, class_instance_uuid, scene_instance_uuid, []);
 
     this.globalObjectInstance.current_port_instance = port_instance;
-    class_instance ? class_instance.port_instance.push(port_instance) : undefined;
-    scene_instance ? scene_instance.port_instances.push(port_instance) : undefined;
+    if (class_instance) class_instance.port_instance.push(port_instance);
+    if (scene_instance) scene_instance.port_instances.push(port_instance);
 
     //push to log file
     this.logger.log("Port Instance " + port_instance.name + " created", "done");
